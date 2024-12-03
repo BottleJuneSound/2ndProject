78aa0ed6f85e0ded34efd72ead780252a3eabc1c
BottleJuneSound <kbjun0320@naver.com>
2024-12-03 11:51:31 +0900

    AddCode
    - 점심시간 백업
    - 플레이어가 전투에 사용할 아이템 사용, 습득하는 아이템 매니저 제작중

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,41 @@
+using TMPro;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public GameObject player;
+    public TMP_Text lightCount;
+    public TMP_Text matcheCount;
+    public TMP_Text potionCount;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GetItem()
+    {
+
+    }
+
+    public void SpendItem()
+    {
+
+    }
+
+    public void OnSpendLight()  // 버튼 입력에 할당할 라이트사용 활성화 함수
+    {
+
+    }
+
+    public void OnSpendMatche() // 버튼 입력에 할당 할 성냥사용 활성화 함수 
+    {
+
+    }
+
+}

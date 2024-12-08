aecb7816fa07639a1c18cd6b5e52c8940baad9fb
BottleJuneSound <kbjun0320@naver.com>
2024-12-08 18:55:13 +0900

    AddCode
    - 사운드 ui구성을 위한 페이더 ~ 사운드 믹서의 값 교환 코드 작성
    - 정상적으로 동작하는 코드를 ambiSlider에 적용 후 커밋

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     public GameObject soundSystemPopup;
-    //public PlayerController player;
-
+    public AudioMixer audioMixer;
+    private float minVolume = -20f;
+    private float maxVolume = 0;
+    public Slider ambiSlider;
     public bool uiPop;
     
     static SoundManager instance;
@@ -27,7 +31,22 @@ public class SoundManager : MonoBehaviour
     {
         uiPop = false;
         soundSystemPopup.SetActive(false);
+        float currentVolume;
+        if (audioMixer.GetFloat("Ambi", out currentVolume)) //시작시 페이더와 믹서의 초기값 설정
+        {
+            ambiSlider.value = MapVolumeToSliderValue(currentVolume);
+            ambiSlider.value = 0.75f;
+        }
+    }
 
+    private float MapVolumeToSliderValue(float volume)  //db를 슬라이더 값으로 변환
+    {
+        return Mathf.InverseLerp(minVolume, maxVolume, volume);
+    }
+
+    private float MapSliderValueToVolume(float sliderValue) //슬라이더 값을 db로 변환
+    {
+        return Mathf.Lerp(minVolume, maxVolume, sliderValue);
     }
 
     public void OnSoundSettingPopup()   //사운드 세팅 팝업 버튼 클릭
@@ -46,13 +65,14 @@ public class SoundManager : MonoBehaviour
             soundSystemPopup.SetActive(false);
             Time.timeScale = 1f;
         }
-
     }
 
-    public void AmbiFader()
+    public void AmbiFader(float sliderValue)
     {
-
+        float volume = Mathf.Lerp(minVolume, maxVolume, sliderValue);
+        audioMixer.SetFloat("Ambi", volume);
     }
+
 
     public void SFXFader()
     {
